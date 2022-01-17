79af31c65625a69184e62027399345947c982ec4
Romeo Folie <romeofolie1@gmail.com>
2022-01-17 14:47:00 +0000

    added in client edit form

--- frontend/src/components/ClientList.vue
+++ frontend/src/components/ClientList.vue
@@ -102,7 +102,7 @@
                                 cols="2"
                                 class="d-flex justify-center align-center"
                               >
-                                <v-icon dense
+                                <v-icon dense color="black"
                                   >mdi-pencil-box-multiple-outline</v-icon
                                 >
                               </v-col>
@@ -111,7 +111,7 @@
                                 cols="2"
                                 class="d-flex justify-center align-center"
                               >
-                                <v-icon dense>mdi-delete</v-icon>
+                                <v-icon dense color="black">mdi-delete</v-icon>
                               </v-col>
                             </v-row>
 
@@ -130,7 +130,7 @@
                                 cols="2"
                                 class="d-flex justify-center align-center"
                               >
-                                <v-icon dense
+                                <v-icon dense color="black"
                                   >mdi-pencil-box-multiple-outline</v-icon
                                 >
                               </v-col>
@@ -139,7 +139,7 @@
                                 cols="2"
                                 class="d-flex justify-center align-center"
                               >
-                                <v-icon dense>mdi-delete</v-icon>
+                                <v-icon dense color="black">mdi-delete</v-icon>
                               </v-col>
                             </v-row>
 
@@ -158,7 +158,7 @@
                                 cols="2"
                                 class="d-flex justify-center align-center"
                               >
-                                <v-icon dense
+                                <v-icon dense color="black"
                                   >mdi-pencil-box-multiple-outline</v-icon
                                 >
                               </v-col>
@@ -167,7 +167,7 @@
                                 cols="2"
                                 class="d-flex justify-center align-center"
                               >
-                                <v-icon dense>mdi-delete</v-icon>
+                                <v-icon dense color="black">mdi-delete</v-icon>
                               </v-col>
                             </v-row>
                           </div>
@@ -184,7 +184,9 @@
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn @click="close" elevation="1"> Cancel </v-btn>
+              <v-btn @click="closeNewDialog" elevation="1" class="mr-4">
+                Cancel
+              </v-btn>
               <v-btn @click="save" elevation="1"> Add client </v-btn>
             </v-card-actions>
           </v-card>
@@ -197,13 +199,198 @@
         :items-per-page="10"
       >
         <template v-slot:item.actions="{item}">
-          <v-btn
-            elevation="1"
-            @click="clickedItem(item.id)"
-            class="mr-4"
-            x-small
-            >Edit</v-btn
-          >
+          <v-dialog v-model="editDialog" max-width="50%">
+            <template v-slot:activator="{on, attrs}">
+              <v-btn
+                elevation="2"
+                @click="clickedItem(item.id)"
+                class="mr-4"
+                x-small
+                v-bind="attrs"
+                v-on="on"
+                >Edit</v-btn
+              >
+            </template>
+            <v-card>
+              <v-card-title>
+                <h3 class="modal-title">Edit Client</h3>
+              </v-card-title>
+
+              <v-divider></v-divider>
+
+              <v-card-text>
+                <v-container class="form-wrap">
+                  <div class="form-group">
+                    <v-row>
+                      <v-col cols="2" class="d-flex justify-end align-center">
+                        <span class="label">Name:</span>
+                      </v-col>
+                      <v-col cols="10">
+                        <input type="text" />
+                      </v-col>
+                    </v-row>
+                  </div>
+
+                  <div class="form-group">
+                    <v-row>
+                      <v-col cols="2" class="d-flex justify-end align-center">
+                        <span class="label">Email:</span>
+                      </v-col>
+                      <v-col cols="10">
+                        <input type="text" />
+                      </v-col>
+                    </v-row>
+                  </div>
+
+                  <div class="form-group">
+                    <v-row>
+                      <v-col cols="2" class="d-flex justify-end align-center">
+                        <span class="label">Phone:</span>
+                      </v-col>
+                      <v-col cols="10">
+                        <input type="text" />
+                      </v-col>
+                    </v-row>
+                  </div>
+
+                  <div class="form-group">
+                    <v-row>
+                      <v-col cols="2" class="d-flex justify-end align-center">
+                        <span class="label">Provider:</span>
+                      </v-col>
+                      <v-col cols="10">
+                        <v-row>
+                          <v-col cols="8">
+                            <input type="text" />
+                          </v-col>
+                          <v-col cols="4">
+                            <v-btn elevation="1" width="100%"
+                              >Add Provider</v-btn
+                            >
+                          </v-col>
+                        </v-row>
+                      </v-col>
+                    </v-row>
+                  </div>
+
+                  <!-- List of providers -->
+                  <div class="form-group">
+                    <v-row>
+                      <v-col cols="2"></v-col>
+                      <v-col cols="10">
+                        <v-row>
+                          <v-col cols="8">
+                            <div class="provider-list">
+                              <v-row no-gutters align="center">
+                                <v-col cols="8">
+                                  <v-checkbox
+                                    v-model="checkbox"
+                                    label="Provider 1"
+                                    dense
+                                    hide-details
+                                    color="black"
+                                  ></v-checkbox>
+                                </v-col>
+
+                                <v-col
+                                  cols="2"
+                                  class="d-flex justify-center align-center"
+                                >
+                                  <v-icon dense color="black"
+                                    >mdi-pencil-box-multiple-outline</v-icon
+                                  >
+                                </v-col>
+
+                                <v-col
+                                  cols="2"
+                                  class="d-flex justify-center align-center"
+                                >
+                                  <v-icon dense color="black"
+                                    >mdi-delete</v-icon
+                                  >
+                                </v-col>
+                              </v-row>
+
+                              <v-row no-gutters align="center">
+                                <v-col cols="8">
+                                  <v-checkbox
+                                    v-model="checkbox"
+                                    label="Provider 1"
+                                    dense
+                                    hide-details
+                                    color="black"
+                                  ></v-checkbox>
+                                </v-col>
+
+                                <v-col
+                                  cols="2"
+                                  class="d-flex justify-center align-center"
+                                >
+                                  <v-icon dense color="black"
+                                    >mdi-pencil-box-multiple-outline</v-icon
+                                  >
+                                </v-col>
+
+                                <v-col
+                                  cols="2"
+                                  class="d-flex justify-center align-center"
+                                >
+                                  <v-icon dense color="black"
+                                    >mdi-delete</v-icon
+                                  >
+                                </v-col>
+                              </v-row>
+
+                              <v-row no-gutters align="center">
+                                <v-col cols="8">
+                                  <v-checkbox
+                                    v-model="checkbox"
+                                    label="Provider 1"
+                                    dense
+                                    hide-details
+                                    color="black"
+                                  ></v-checkbox>
+                                </v-col>
+
+                                <v-col
+                                  cols="2"
+                                  class="d-flex justify-center align-center"
+                                >
+                                  <v-icon dense color="black"
+                                    >mdi-pencil-box-multiple-outline</v-icon
+                                  >
+                                </v-col>
+
+                                <v-col
+                                  cols="2"
+                                  class="d-flex justify-center align-center"
+                                >
+                                  <v-icon dense color="black"
+                                    >mdi-delete</v-icon
+                                  >
+                                </v-col>
+                              </v-row>
+                            </div>
+                          </v-col>
+                          <v-col cols="4"></v-col>
+                        </v-row>
+                      </v-col>
+                    </v-row>
+                  </div>
+                </v-container>
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn @click="closeEditDialog" elevation="1" class="mr-4">
+                  Cancel
+                </v-btn>
+                <v-btn @click="save" elevation="1"> Update client </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
           <v-btn elevation="1" color="red" dark x-small>Delete</v-btn>
         </template>
       </v-data-table>
@@ -277,12 +464,15 @@ export default {
     clickedItem(itemId) {
       console.log("item id ", itemId);
     },
-    close() {
+    closeNewDialog() {
       this.newDialog = false;
       // this.$nextTick(() => {
       //   this.editedItem = Object.assign({}, this.defaultItem)
       //   this.editedIndex = -1
       // })
+    },
+    closeEditDialog() {
+      this.editDialog = false;
     },
   },
 };
@@ -334,5 +524,9 @@ button {
   padding: 10px 15px;
   display: flex;
   flex-direction: column;
+}
+
+i {
+  cursor: pointer !important;
 }
 </style>
